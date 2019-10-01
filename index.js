581d6ef9bf2962138f83fdeb62f4b8fba3ed3b82
elijahmm17 <elijahmm17@gmail.com>
2019-10-01 01:45:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,27 +6,27 @@ function nestedTarget() {
   return document.querySelector('#nested .target');
 }
 
-// function increaseRankBy(n) {
-//   const lists = document.querySelectorAll('.ranked-list');
-//   for (let i = 0; i < lists.length; i++) {
-//     let items = lists[i].children;
-//     for (let j = 0; j < items.length; j++) {
-//       items[j].innerHTML = parseInt(items[j].innerHTML) + n;
-//     }
-//   }
-// }
-
-function increaseRankBy( n ) {
-  const rankedLists = document.querySelectorAll( '.ranked-list' )
-
-  for ( let i = 0, l = rankedLists.length; i < l; i++ ) {
-    let children = rankedLists[ i ].children
-
-    for ( let j = 0, k = children.length; j < k; j++ ) {
-      children[ j ].innerHTML = parseInt( children[ j ].innerHTML ) + n
+function increaseRankBy(n) {
+  const lists = document.querySelectorAll('.ranked-list');
+  for (let i = 0; i < lists.length; i++) {
+    let items = lists[i].children;
+    for (let j = 0; j < items.length; j++) {
+      items[j].innerHTML = parseInt(items[j].innerHTML) + n;
     }
   }
 }
+
+// function increaseRankBy( n ) {
+//   const rankedLists = document.querySelectorAll( '.ranked-list' )
+
+//   for ( let i = 0, l = rankedLists.length; i < l; i++ ) {
+//     let children = rankedLists[ i ].children
+
+//     for ( let j = 0, k = children.length; j < k; j++ ) {
+//       children[ j ].innerHTML = parseInt( children[ j ].innerHTML ) + n
+//     }
+//   }
+// }
 
 function deepestChild() {
   return document.querySelector('#grand-node div div div div');
