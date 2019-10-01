0e7d2b8543e9548c339e2bb2df93c86f4f7d943c
elijahmm17 <elijahmm17@gmail.com>
2019-10-01 01:44:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ function nestedTarget() {
   return document.querySelector('#nested .target');
 }
 
-function increaseRankBy(n) {
-  const lists = document.querySelectorAll('.ranked-list');
-  for (let i; i < lists.length; i++) {
-    let items = lists[i].children;
-    for (let j; j < items.length; j++) {
-      items[j].innerHTML = parseInt(items[j].innerHTML) + n;
-    }
-  }
-}
+// function increaseRankBy(n) {
+//   const lists = document.querySelectorAll('.ranked-list');
+//   for (let i; i < lists.length; i++) {
+//     let items = lists[i].children;
+//     for (let j; j < items.length; j++) {
+//       items[j].innerHTML = parseInt(items[j].innerHTML) + n;
+//     }
+//   }
+// }
 
 function deepestChild() {
   return document.querySelector('#grand-node div div div div');
